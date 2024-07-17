d4846ea43a609bd30041aefce8c3da927243f80e
ljllanosl <ljllanosl91@gmail.com>
2024-07-17 02:27:22 -0500

    Add authentication functionality to NavBar

--- src/app/layout/nav/NavBar.tsx
+++ src/app/layout/nav/NavBar.tsx
@@ -1,8 +1,12 @@
 import { Button, Navbar } from 'flowbite-react'
 import { NavLink } from 'react-router-dom'
 import SignedInMenu from './SignedInMenu'
+import { useState } from 'react'
+import SignedOutButtons from './SignedOutButtons'
 
 export default function NavBar() {
+  const [auth, setAuth] = useState(false)
+
   return (
     // Simple navbar with tailwind
     <header className='bg-gradient-to-br from-blue-900 from-0% via-cyan-700 via-70% to-teal-500 to-90% fixed top-0 left-0 right-0 z-10'>
@@ -15,8 +19,7 @@ export default function NavBar() {
           <span className='font-medium text-xl' >Events</span>
         </NavLink>
         <Button as={NavLink} to='/createEvent'>Create Event</Button>
-        <SignedInMenu/>
-        {/* <SignedOutButtons/> */}
+        {auth ? <SignedInMenu setAuth={setAuth} /> : <SignedOutButtons setAuth={setAuth} />}
       </Navbar>
     </header>
 
--- src/app/layout/nav/SignedOutButtons.tsx
+++ src/app/layout/nav/SignedOutButtons.tsx
@@ -1,9 +1,13 @@
 import { Button } from 'flowbite-react'
 
-export default function SignedOutButtons() {
+interface Props{
+  setAuth: (auth: boolean) => void
+}
+
+export default function SignedOutButtons({setAuth}: Props) {
   return (
     <>
-      <Button className='bg-transparent border-white'>Login</Button>
+      <Button className='bg-transparent border-white' onClick={() => setAuth(true)}>Login</Button>
       <Button className='bg-transparent border-white'>Register</Button>
     </>
   )
